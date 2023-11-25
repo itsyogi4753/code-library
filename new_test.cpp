6d506863e0b6e0266fda320c7b4c57c22147329f
itsyogi4753 <143088494+itsyogi4753@users.noreply.github.com>
2023-11-25 19:00:58 +0530

    added an add function

--- new_test.cpp
+++ new_test.cpp
@@ -1,9 +1,16 @@
 #include<iostream>
 using namespace std;
 
+int add(int a, int b) {
+	return a + b;
+}
+
 int main() {
 	int var;
 	cin >> var;
 	cout << var << endl;
+	int var2;
+	cin >> var2; 
+	cout << add(var, var2) << endl; 
 	return 0;
 }
